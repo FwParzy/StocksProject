2b45335cb26a3758d439349bd9ccd4a4088d7f82
FwParzy <guardianiisaac@gmail.com>
2022-11-03 14:52:47 -0400

    created login class

---
+++ Stonks.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Stonks {
+  public static void main(String[] args) {
+
+    // Calling the login class and logging in
+    Login user = new Login();
+    user.login();
+
+    System.out.println("We be doing things");
+  }
+}
