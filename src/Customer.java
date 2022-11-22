54f8c625614dcbae0e149c51b83224e1aaa48bf1
FwParzy <guardianiisaac@gmail.com>
2022-11-22 16:10:21 -0500

    Paired with Elijah on Main class

--- src/Customer.java
+++ src/Customer.java
@@ -1,79 +1,63 @@
 public class Customer {
 
   private String name;
-
   private double amount;
-
   private String material;
-
   private double price;
-
   private double totalPrice;
 
   public Customer() {
 
   }
 
-  public Customer(String name, String material) {
-    this.material = material;
+  public Customer(String name, String material, double amount, double price) {
     this.name = name;
-
+    this.material = material;
+    this.amount = amount;
+    this.price = price;
   }
 
   public void setName(String name) {
-
     this.name = name;
-
   }
 
   public String getName() {
-
     return name;
   }
 
   public void setMaterial(String material) {
-
     this.material = material;
-
   }
 
   public String getMaterial() {
-
     return material;
   }
 
-  public Customer(double amount, double price) {
-
-    this.amount = amount;
-    this.price = price;
-
-  }
-
   public void setAmount(double amount) {
-
     this.amount = amount;
-
   }
 
   public double getAmount() {
-
     return amount;
-
   }
 
   public void setPrice(double price) {
-
     this.price = price;
-
   }
 
   public double getPrice() {
-
     return price;
   }
 
   public double getTotalPrice() {
-
     return price * amount;
+  }
+
+  public void display(){
+    System.out.println("Customer:  "+this.name);
+    System.out.println("Material:  "+this.material);
+    System.out.println("Buy Price: "+this.price);
+    System.out.println("Quantity:  "+this.amount);
+    System.out.println();
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,39 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
   public static void main(String[] args) {
 
-    // Calling the login class and logging in
+    Scanner in = new Scanner(System.in);
     Login user = new Login();
+    InventoryManager inventory = new InventoryManager();
+    CustomerManager customers = new CustomerManager();
+    String answer = "";
+
+    // Calling the login class and logging in
     user.login();
 
-    System.out.println("We be doing things");
+    System.out.println("Welcome to foo Inventory Management program");
 
 
+    while (!answer.equals("quit")) {
+      System.out.println();
+      System.out.println("What would you like to manage?");
+      System.out.println("Inventory, Customers, TBA, quit");
+      answer = in.next();
+
+      if (answer.equals("inventory")) {
+        inventory.runInputCheck();
+
+      } else if (answer.equals("customer") ) {
+        customers.runInputCheck();
+        
+      }
+      else {
+        System.out.println("something broke");
+      }
+
+    }
+    System.out.println("Thanks for using foo Inventory Management");
   }
 }
